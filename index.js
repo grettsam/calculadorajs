7bba37926f3b7a5804b4c60d14aaa61c33e08dfd
Sebastian Infante <sebastian.infante.dg@gmail.com>
2021-07-30 15:28:23 -0400

    add id

---
+++ index.js
@@ -0,0 +1,22 @@
+let cero = () => (document.getElementById("input").value += 0);
+let uno = () => (document.getElementById("input").value += 1);
+let dos = () => (document.getElementById("input").value += 2);
+let tres = () => (document.getElementById("input").value += 3);
+let cuatro = () => (document.getElementById("input").value += 4);
+let cinco = () => (document.getElementById("input").value += 5);
+let seis = () => (document.getElementById("input").value += 6);
+let siete = () => (document.getElementById("input").value += 7);
+let ocho = () => (document.getElementById("input").value += 8);
+let nueve = () => (document.getElementById("input").value += 9);
+let punto = () => (document.getElementById("input").value += '.');
+
+let suma = () => (document.getElementById("input").value += "+");
+let restar = () => (document.getElementById("input").value += "-");
+let multiplicar = () => (document.getElementById("input").value += "*");
+let dividir = () => (document.getElementById("input").value += '/');
+
+
+let limpiar = () => (document.getElementById("input").value = '');
+let calcular = () => {
+  
+}
